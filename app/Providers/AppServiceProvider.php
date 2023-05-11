5e70d76ccd3ba05634fb5b4dc28b83a179bb0ccb
Nadirsah <isayevnadirsah26@gmail.com>
2023-05-11 11:39:11 +0400

    favicon

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 use Validator;
+use App\Models\AyarlarModel;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        view()->share('profil',AyarlarModel::first()); 
+
         Paginator::useBootstrap();
 
         Validator::extend('recaptcha', 'App\\Validators\\ReCaptcha@validate');
